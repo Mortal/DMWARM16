120e1310445a9e2fcb8a9f49d47e1c71c15bcf06
Mathias Rav <rav@cs.au.dk>
2016-09-17 12:02:19 +0200

    wip

--- bdaycake.py
+++ bdaycake.py
@@ -1,21 +1,12 @@
 from collections import Counter
 n = int(input().strip())
-bdays = [input() for _ in range(n)]
-bdays = Counter((int(d[2:]) - 1) * 30 + int(d[:2]) - 1 for d in bdays)
-dates = [m * 30 + d for m in range(12) for d in range(30)]
-counts = [bdays.get(0, 0)]
-for md in dates[1:]:
-    counts.append(counts[-1] + bdays.get(md, 0))
-best = float('inf')
-bestdate = None
-for md in dates:
-    m, d = divmod(md, 30)
-    if m == 11:
-        miss = counts[d-1] + (counts[-1] - counts[md-1])
-    else:
-        miss = counts[md+30-1] - (counts[md-1] if md else 0)
-    if miss < best:
-        best = miss
-        bestdate = md
+counts = [0] * 720
+for _ in range(n):
+    dm = input().strip()
+    i = (int(d[2:]) - 1) * 30 + int(d[:2]) - 1
+    counts[i] += 1
+    counts[i+360] += 1
+bestdate = min(range(360),
+               key=lambda md: sum(counts[i] for i in range(md, md + 30)))
 m, d = divmod(bestdate, 30)
 print('%02d%02d' % (d+1, m+1))
