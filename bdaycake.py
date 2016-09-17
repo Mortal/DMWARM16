71c1bc2e53f92a7095967eed1c5edbf3a12f3fea
Mathias Rav <rav@cs.au.dk>
2016-09-17 11:57:40 +0200

    wip

--- bdaycake.py
+++ bdaycake.py
@@ -2,13 +2,10 @@ from collections import Counter
 n = int(input().strip())
 bdays = [input() for _ in range(n)]
 bdays = Counter((int(d[2:]) - 1) * 30 + int(d[:2]) - 1 for d in bdays)
-# print(bdays)
 dates = [m * 30 + d for m in range(12) for d in range(30)]
 counts = [bdays.get(0, 0)]
 for md in dates[1:]:
     counts.append(counts[-1] + bdays.get(md, 0))
-    # m, d = divmod(md, 30)
-    # print(m+1, d+1, counts[-1])
 best = float('inf')
 bestdate = None
 for md in dates:
@@ -17,7 +14,6 @@ for md in dates:
         miss = counts[d-1] + (counts[-1] - counts[md-1])
     else:
         miss = counts[md+30-1] - (counts[md-1] if md else 0)
-    # print(m+1, d+1, miss)
     if miss < best:
         best = miss
         bestdate = md
--- pachinko.py
+++ pachinko.py
@@ -1,34 +1,28 @@
-import sys
-lines = iter(sys.stdin)
-w, h, b, n = map(int, next(lines).split())
-board = [next(lines).strip()[1:-1] for _ in range(h-1)]
-buckets = next(lines).strip()[1:-1]
-next(lines)  # Frame
+w, h, b, n = map(int, input().split())
+board = [input().strip()[1:-1] for _ in range(h-1)]
+buckets = input().strip()[1:-1]
+input()  # Frame
 bucket_scores = {k: int(v) for _ in range(b)
-                 for k, v in [next(lines).split()]}
-queries = list(map(int, next(lines).split()))
+                 for k, v in [input().split()]}
+queries = list(map(int, input().split()))
+
+def fill(values):
+    for i, v in enumerate(values):
+        if v is None:
+            if i == 0:
+                values[i] = values[i+1]
+            elif i+1 == len(values):
+                values[i] = values[i-1]
+            else:
+                values[i] = (values[i-1] + values[i+1]) / 2
+    return values
+
 scores = []
 # Last line
-scores.append([None if c == '|' else bucket_scores[c] for c in buckets])
-for i, v in enumerate(scores[-1]):
-    if v is None:
-        if i == 0:
-            scores[-1][i] = scores[-1][i+1]
-        elif i+1 == len(scores[-1]):
-            scores[-1][i] = scores[-1][i-1]
-        else:
-            scores[-1][i] = (scores[-1][i-1] + scores[-1][i+1]) / 2
+scores.append(
+    fill([None if c == '|' else bucket_scores[c] for c in buckets]))
 for row in reversed(board):
-    scores.append([])
-    for i, c in enumerate(row):
-        if c == '.':
-            if i == 0:
-                scores[-1].append(scores[-2][i+1])
-            elif i+1 == len(row):
-                scores[-1].append(scores[-2][i-1])
-            else:
-                scores[-1].append((scores[-2][i-1] + scores[-2][i+1]) / 2)
-        else:
-            scores[-1].append(scores[-2][i])
+    scores.append(
+        fill([None if c == '.' else scores[-1][i] for i, c in enumerate(row)]))
 scores.reverse()
 print(sum(scores[0][i-1] for i in queries))
